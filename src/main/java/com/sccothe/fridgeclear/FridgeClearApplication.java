1906efd84b6bc01605e0bbe1e6e3d495f65f50ea
Scott He <sccothe@88vip.site>
2026-07-31 08:15:22 +0800

    Initial commit: Spring Boot FridgeClear project

---
+++ src/main/java/com/sccothe/fridgeclear/FridgeClearApplication.java
@@ -0,0 +1,13 @@
+package com.sccothe.fridgeclear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeClearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeClearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sccothe/fridgeclear/FridgeClearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sccothe.fridgeclear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeClearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
